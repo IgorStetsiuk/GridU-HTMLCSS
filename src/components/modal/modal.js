54829d88d14435ca2dde331b5a8bfa7c705a1271
Igor Stetsiuk <igorstetsuk@gmail.com>
2018-01-27 00:10:45 +0200

    30% done

---
+++ src/components/modal/modal.js
@@ -0,0 +1,12 @@
+const modal = document.getElementById('modal');
+
+// const triggerButton = document.getElementById('open-modal');
+
+document.body.onclick = function (e) {
+  const target = e.target;
+  if(target.nodeType='BUTTON' && target.classList.contains('open-modal')){
+
+  }
+};
+
+
